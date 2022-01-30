4df2537611e731e2c953a8843ad4e4f7b9bf2c98
ianwesleyarmstrong <ianwesleyarmstrong@gmail.com>
2022-01-30 13:05:41 -0600

    remove auth

--- main.tf
+++ main.tf
@@ -21,9 +21,6 @@ module "mlflow" {
   service_type = var.mlflow_service_type
 }
 
-
-
-
 module "feast" {
   count = var.install_feast ? 1 : 0
 
@@ -43,40 +40,4 @@ module "seldon" {
   count     = var.install_seldon ? 1 : 0
   source    = "./modules/seldon"
   namespace = var.seldon_namespace
-}
-
-
-module "ambassador" {
-  count     = var.ambassador_enabled ? 1 : 0
-  source    = "./modules/ambassador"
-  namespace = var.ambassador_namespace
-
-  aws                 = var.aws
-  tls_certificate_arn = var.tls_certificate_arn
-
-  hostname                  = var.hostname
-  tls                       = var.protocol == "https" ? true : false
-  enable_ory_authentication = var.enable_ory_authentication
-}
-
-
-
-module "ory" {
-  count              = var.enable_ory_authentication ? 1 : 0
-  source             = "./modules/ory"
-  namespace          = var.ory_namespace
-  cookie_secret      = var.ory_kratos_cookie_secret
-  kratos_db_password = var.ory_kratos_db_password
-  oauth2_providers   = var.oauth2_providers
-
-  hostname = var.hostname
-  protocol = var.protocol
-
-  enable_registration      = var.enable_registration_page
-  enable_password_recovery = var.enable_password_recovery
-  enable_verification      = var.enable_verification
-  smtp_connection_uri      = var.smtp_connection_uri
-  smtp_from_address        = var.smtp_from_address
-
-  access_rules_path = var.access_rules_path
 }
