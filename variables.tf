d461b381c9d0ebb0aaa9faca4c6f836aa167e2b2
ianwesleyarmstrong <ianwesleyarmstrong@gmail.com>
2022-01-30 13:43:44 -0600

    same

--- variables.tf
+++ variables.tf
@@ -146,25 +146,25 @@ variable "seldon_namespace" {
   default = "seldon"
 }
 
-variable "oauth2_providers" {
-  //  Configure multiple Oauth2 providers.
-  //  example:
-  //  [{
-  //    provider = github
-  //    client_id = change_me
-  //    client_secret = change_me
-  //    tenant = null
-  //  }]
-  //  If you're using GitHub, Google or Facebook, tenant won't be needed, so please set
-  //  it as null or an empty string. It is required for AzureAd
-  type = list(object({
-    provider      = string
-    client_id     = string
-    client_secret = string
-    tenant        = string
-  }))
-  description = "OAuth2 Providers credentials"
-}
+# variable "oauth2_providers" {
+#   //  Configure multiple Oauth2 providers.
+#   //  example:
+#   //  [{
+#   //    provider = github
+#   //    client_id = change_me
+#   //    client_secret = change_me
+#   //    tenant = null
+#   //  }]
+#   //  If you're using GitHub, Google or Facebook, tenant won't be needed, so please set
+#   //  it as null or an empty string. It is required for AzureAd
+#   type = list(object({
+#     provider      = string
+#     client_id     = string
+#     client_secret = string
+#     tenant        = string
+#   }))
+#   description = "OAuth2 Providers credentials"
+# }
 
 variable "smtp_connection_uri" {
   description = "SMTP Connection for Ory"
